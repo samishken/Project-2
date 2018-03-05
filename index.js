a1524748cfbee935b5ee4d4cc38b16ede22b8581
SAMUEL HAILE <samishken@gmail.com>
2018-03-05 11:04:28 -0500

    install and setup handlebars

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-    const express = require('express')
+const express = require('express')
+const hbs = require(‘hbs’) 
+const app = express()
 
-    const app = express()
+app.set('view engine', 'hbs')
 
+app.get('/', (req, res) => {
+  res.render('index')
+})
 
-    app.get('/', (req, res) => {
-      res.send('Project TWO')
-    })
+app.get('/', (req, res) => {
+  res.send('Project TWO')
+})
 
-    // start our server
-    app.listen(3000, () => console.log('This is working...Project 2'))
+// start our server
+app.listen(3000, () => console.log('This is working...Project 2'))
