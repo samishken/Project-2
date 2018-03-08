cd73be437a646cedc6ff749306dfb85cba3e44f6
SAMUEL HAILE <samishken@gmail.com>
2018-03-07 19:59:59 -0500

    update CRUD

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ const parser = require('body-parser')
 const Luxury = require('./models/Luxury')
 const luxuryCarsController = require('./controllers/luxurycars')
 const methodOverride = require('method-override')
+const path = require('path')
 
 const app = express()
 
+app.use(express.static(path.join(__dirname, 'public')))
 app.set('view engine', 'hbs')
 app.use(parser.urlencoded({ extended: true }))
 app.use(express.static('public'))
+app.use(express.static('.'))
 app.use(methodOverride('_method'))
 
 app.get('/', (req, res) => {
